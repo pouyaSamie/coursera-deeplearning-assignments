4899d2aa45e19540ba3f54332623736258f155d2
pouya samie <pouya.samie@gmail.com>
2022-12-11 12:33:36 +0330

    add projects

---
+++ Logistic_Regression/lr_utils.py
@@ -0,0 +1,20 @@
+import numpy as np
+import h5py
+    
+    
+def load_dataset():
+    base_address='../coursera/deep-learning/1-neural-networks-and-deep-learning/2-logistic-regression-as-a-neural-network/'
+    train_dataset = h5py.File(base_address + 'datasets/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+    test_dataset = h5py.File(base_address + 'datasets/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
